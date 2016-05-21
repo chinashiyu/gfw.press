42c734bf197ba02705ad5402d2912180a1b8c11e
chinashiyu <chinashiyu@gfw.press>
2016-05-21 22:37:10 +0800

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,6 +1,6 @@
 cd `dirname $0`
 
-_java="java -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8 -Duser.timezone=Asia/Shanghai -Xms256M -Xmx256M -Xmn192M -classpath `find ./lib/*.jar | xargs echo | sed 's/ /:/g'`:./bin"
+_java="java -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8 -Duser.timezone=Asia/Shanghai -Xms256M -Xmx256M -classpath `find ./lib/*.jar | xargs echo | sed 's/ /:/g'`:./bin"
 
 _pack="press.gfw"
 
