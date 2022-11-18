c602d5e4001f802b7fd85a4aa0675e64f9aae4cd
chinashiyu <chinashiyu@gfw.press>
2022-11-18 17:24:09 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -10,6 +10,6 @@ export PATH=$JAVA_HOME/bin:$PATH
 
 _pack="press.gfw"
 
-kill -9 `jps -l |grep $_pack.Server |awk '{print $1}'` > null 2>&1
+kill -9 `jps -l |grep $_pack.Server |awk '{print $1}'` > /dev/null 2>&1
 
 rm -f server.lock
